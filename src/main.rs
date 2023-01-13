c81edf2ff8f0147124fedff09aecf732f0a66480
emanuele-em <micheletti.emanuele@hotmail.com>
2023-01-13 23:46:03 +0100

    package manifest

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use calendar_calculator::CalendarCalculator;
-
-fn main() {
-    let first = "2004-02-29 10:00:00";
-    let second = "1997-07-12 10:00:00";
-    println!("{}", first.distance_between(second));
-    let (y, m, d, _, _, _) = first.add_days(-2423);
-    println!("{}-{}-{}", y, m, d);
-}
